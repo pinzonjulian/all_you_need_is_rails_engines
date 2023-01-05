54d3dd399f0dbe4ab99c9a27d4d28c6185e10b32
pinzonjulian <julianpinzoneslava@gmail.com>
2023-01-05 12:16:01 +1100

    [JS importmap] Configure importmap for the rider_app engine
    The configuration showcased here is meant to mimic 100% the structure and feel of a vanilla rails app.
    For example, when a controller is declared in html it's declared without any prefixes.
    
    I believe the folder configuration "javascript/rider_app" is necessary to distinguish importmap setups between engines.
    
    It's very important to properly configure the importmap file to load stimulus controllers correctly. This is a two step process:
    1. Tell the rails asset manager (Propshaft in this case) to compile assets for the engine using an initializer in the engine.rb file
    2. Make sure to add the proper arguments to `pin_all_from`
      - path: use `File.expand_path(..., __dir__)` with the path to the controllers folder
      - under: THIS IS VERY IMPORTANT and took me a long while to figure out. Basically, this argument prefixes the file name to make sure it's correctly found. For example, if the file name is `hello_controller.js` the "under" argument will prefix it so it's correctly found after assets are compiled: "rider_app/controllers/hello_controller.js". This works in conjunction with the point 1 above.

--- packages/rider_app/lib/rider_app/engine.rb
+++ packages/rider_app/lib/rider_app/engine.rb
@@ -1,5 +1,21 @@
 module RiderApp
   class Engine < ::Rails::Engine
     isolate_namespace RiderApp
+
+    initializer "rider_app.assets" do |app|
+      # add app/javascript in engine to mimic the default structure
+      # of a vanilla Rails application
+      app.config.assets.paths << Engine.root.join("app/javascript")
+    end
+
+    initializer "rider_app.importmap", before: "importmap" do |app|
+      # Allows for composing importmaps from engines
+      # https://github.com/rails/importmap-rails#composing-import-maps
+      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+
+      # Add the app/javascript folder of the engine to the cache sweeping mechanism
+      # https://github.com/rails/importmap-rails#sweeping-the-cache-in-development-and-test
+      app.config.importmap.cache_sweepers << Engine.root.join("app/javascript")
+    end
   end
 end
